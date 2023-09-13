c1c4489701b9dd631e5ebd9df80ed5f61fd78676
srlee056 <40015447+srlee056@users.noreply.github.com>
2023-09-14 00:07:05 +0900

    [Silver II] Title: 친구, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.py
+++ 백준/Silver/1058. 친구/친구.py
@@ -1,14 +1,10 @@
 num = int(input())
 
-# num * num 2차 리스트 or 저장소 선언
 friendList = ['' for _ in range(num)]
 
 for i in range(num):
    friendList[i] = input()
 
-#numberOfFriends = [0 for _ in range(num)]
-#numberOfFriends = [friendList[j].count('Y') for j in range(num)]
-#sndFr = [0 for _ in range(num)]
 frDist = [[0 for _ in range(num)] for _ in range(num)]
 fstFrSet = [set() for _ in range(num)]
 
@@ -16,7 +12,6 @@ for j in range(num):
    fstFrSet[j] = set([k for k in range(num) if (friendList[j][k]=='Y')])  
 
 sndFrSet = fstFrSet.copy()
-#print(fstFrSet)
 
 for j in range(num):
    for x in fstFrSet[j]:
