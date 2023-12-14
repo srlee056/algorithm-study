03d3c3b178b14086ea4db04dcffe55b8947e2b89
이서림 <40015447+srlee056@users.noreply.github.com>
2023-12-14 11:29:36 +0900

    [Bronze I] Title: 임시 반장 정하기, Time: 212 ms, Memory: 64960 KB -BaekjoonHub

--- 백준/Bronze/1268. 임시 반장 정하기/임시 반장 정하기.py
+++ 백준/Bronze/1268. 임시 반장 정하기/임시 반장 정하기.py
@@ -3,20 +3,12 @@ from collections import Counter
 
 def main():
     num_of_students = int(input())
-    # num_of_students = 5
+
     groups = []
-    """
-    predefined_groups = [
-        [2, 3, 1, 7, 3],
-        [4, 1, 9, 6, 8],
-        [5, 5, 2, 4, 4],
-        [6, 5, 2, 6, 7],
-        [8, 4, 2, 2, 2],
-    ]
-    """
+
     for i in range(num_of_students):
         group = map(int, input().split())
-        # group = predefined_groups[i]
+
         groups.append(list(group))
 
     common_groups = {}
