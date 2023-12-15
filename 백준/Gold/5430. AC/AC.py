bc2c8539770a8b7aaaa1844775ee9a4ade796bdc
이서림 <40015447+srlee056@users.noreply.github.com>
2023-12-15 11:15:58 +0900

    [Gold V] Title: AC, Time: 224 ms, Memory: 40312 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,11 +1,32 @@
 from collections import deque
-import itertools
+
+
+def process_elements(funcs, elements, is_reversed):
+    left, right = 0, len(elements) - 1
+
+    for func in funcs:
+        if func == "R":
+            is_reversed = not is_reversed
+        elif func == "D":
+            if is_reversed:
+                right -= 1
+            else:
+                left += 1
+
+    if left > right + 1:
+        return "error"
+    else:
+        result = (
+            list(reversed(elements[left : right + 1]))
+            if is_reversed
+            else elements[left : right + 1]
+        )
+        return "[" + ",".join(map(str, result)) + "]"
 
 
 def main():
     num_of_tests = int(input())
 
-    test_cases = []
     for i in range(num_of_tests):
         funcs = input()
         num_of_elements = int(input())
@@ -14,35 +35,8 @@ def main():
             elements = list(elements)
         else:
             elements = []
-        test_cases.append((deque(funcs), num_of_elements, elements))
 
-    for funcs, num_of_elements, elements in test_cases:
-        left, right = 0, num_of_elements - 1
-        is_reversed = False
-
-        while funcs:
-            func = funcs.popleft()
-
-            if func == "R":
-                is_reversed = not is_reversed
-
-            elif func == "D":
-                if is_reversed:
-                    right -= 1
-                else:
-                    left += 1
-
-        if left > right + 1:
-            print("error")
-
-        else:
-            result = (
-                list(reversed(elements[left : right + 1]))
-                if is_reversed
-                else elements[left : right + 1]
-            )
-            result = "[" + ",".join(map(str, result)) + "]"
-            print(result)
+        print(process_elements(deque(funcs), elements, False))
 
 
 if __name__ == "__main__":
