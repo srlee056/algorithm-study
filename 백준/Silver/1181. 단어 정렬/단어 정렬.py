7910be072daf84fde87842449ba3a31074f01529
이서림 <40015447+srlee056@users.noreply.github.com>
2023-12-17 21:49:07 +0900

    [Silver V] Title: 단어 정렬, Time: 92 ms, Memory: 35732 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,14 +1,10 @@
+import sys
 
-n = int(input())
-
-words = []
-for _ in range(n):
-    words.append(input())
+n = int(sys.stdin.readline())
+words = [sys.stdin.readline().rstrip() for i in range(n)]
 
 words = list(set(words))
 words.sort(key=lambda x: (len(x), x))
 
 for word in words:
     print(word)
-
-
