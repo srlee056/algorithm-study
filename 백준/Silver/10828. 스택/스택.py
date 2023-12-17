df8602b2b3c084bc4da5bac7065f9988a2ef7f9d
이서림 <40015447+srlee056@users.noreply.github.com>
2023-12-17 23:21:40 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,27 +1,33 @@
 import sys
 
-n = int(sys.stdin.readline())
 
+def push(x):
+    stack.append(x)
+
+
+def pop():
+    print(stack.pop() if stack else -1)
+
+
+def size():
+    print(len(stack))
+
+
+def empty():
+    print(0 if stack else 1)
+
+
+def top():
+    print(stack[-1] if stack else -1)
+
+
+n = int(sys.stdin.readline())
 stack = []
+command_dict = {"push": push, "pop": pop, "size": size, "empty": empty, "top": top}
+
 for _ in range(n):
     commands = sys.stdin.readline().rstrip().split()
-
     if commands[0] == "push":
-        stack.append(commands[1])
-    elif commands[0] == "pop":
-        if len(stack):
-            print(stack.pop())
-        else:
-            print(-1)
-    elif commands[0] == "size":
-        print(len(stack))
-    elif commands[0] == "empty":
-        if len(stack):
-            print(0)
-        else:
-            print(1)
-    elif commands[0] == "top":
-        if len(stack):
-            print(stack[-1])
-        else:
-            print(-1)
+        command_dict[commands[0]](int(commands[1]))
+    else:
+        command_dict[commands[0]]()
