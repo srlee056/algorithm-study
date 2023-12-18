3fa3d55299257ca87fb082b651402ad9d68b05fa
이서림 <40015447+srlee056@users.noreply.github.com>
2023-12-18 11:40:26 +0900

    [Silver III] Title: 1로 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,5 +1,11 @@
 import sys
 
+
+def update_counts(v, value_with_counts, count):
+    old_count = value_with_counts.get(v, count)
+    value_with_counts[v] = min(old_count, count)
+
+
 n = int(sys.stdin.readline())
 
 value_with_counts = {n: 0}
@@ -12,19 +18,13 @@ while value_with_counts:
         break
 
     if v % 3 == 0:
-        old_count = value_with_counts.get(v // 3, c + 1)
-        value_with_counts[v // 3] = min(old_count, c + 1)
+        update_counts(v // 3, value_with_counts, c + 1)
     elif v % 3 == 1:
-        old_count = value_with_counts.get((v - 1) // 3, c + 2)
-        value_with_counts[(v - 1) // 3] = min(old_count, c + 2)
+        update_counts((v - 1) // 3, value_with_counts, c + 2)
     else:
-        old_count = value_with_counts.get((v - 2) // 3, c + 3)
-        value_with_counts[(v - 2) // 3] = min(old_count, c + 3)
+        update_counts((v - 2) // 3, value_with_counts, c + 3)
 
     if v % 2 == 0:
-        old_count = value_with_counts.get(v // 2, c + 1)
-        value_with_counts[v // 2] = min(old_count, c + 1)
+        update_counts(v // 2, value_with_counts, c + 1)
     else:
-        old_count = value_with_counts.get((v - 1) // 2, c + 2)
-        value_with_counts[(v - 1) // 2] = min(old_count, c + 2)
-
+        update_counts((v - 1) // 2, value_with_counts, c + 2)
