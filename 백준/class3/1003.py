02dc91f97294d6ee6c4989222cabf8437350c17e
Lee Sarah <imsolem1226@gmail.com>
2023-12-18 10:30:21 +0900

    Update 1003.py

--- 백준/class3/1003.py
+++ 백준/class3/1003.py
@@ -4,19 +4,16 @@ import sys
 # fib_print_zero = 1, 0, 1, 1, 2, ..
 # fib_print_one = 0, 1, 1, 2, 3, ..
 def fib_print(n):
-    if n == 0:
-        return [1, 0]
-    elif n == 1:
-        return [0, 1]
-    else:
-        n_1 = fib_print(n - 1)
-        n_2 = fib_print(n - 2)
-        return [n_1[0] + n_2[0], n_1[1] + n_2[1]]
+    for i in range(2, n + 1):
+        if i not in fib_prints:
+            fib_prints[i] = [fib_prints[i - 1][1], sum(fib_prints[i - 1])]
 
 
 test_cases = int(sys.stdin.readline())
 
+fib_prints = {0: [1, 0], 1: [0, 1]}
+
 for _ in range(test_cases):
     n = int(sys.stdin.readline())
-    fib_prints = map(str, fib_print(n))
-    print(" ".join(fib_prints))
+    fib_print(n)
+    print(" ".join(map(str, fib_prints[n])))
