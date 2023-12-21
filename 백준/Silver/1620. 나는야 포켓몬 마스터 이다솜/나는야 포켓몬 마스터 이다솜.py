79ce37d825e944afab6ab988c18845d49ead5e57
이서림 <40015447+srlee056@users.noreply.github.com>
2023-12-21 02:02:34 +0000

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 232 ms, Memory: 50848 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -10,11 +10,13 @@ for _ in range(n):
 pokemons_dict = {}
 
 for i, pokemon in enumerate(pokemons):
-    pokemons_dict[str(int(i) + 1)] = pokemon
-    pokemons_dict[pokemon] = str(int(i) + 1)
+    pokemons_dict[pokemon] = str(i + 1)
 
 
 for _ in range(m):
     question = sys.stdin.readline().rstrip()
 
-    print(pokemons_dict[question])
+    if question.isdigit():
+        print(pokemons[int(question) - 1])
+    else:
+        print(pokemons_dict[question])
