f63e00a8ac894432bd93ef654009f0a98a0ac5c4
Lee Sarah <imsolem1226@gmail.com>
2024-01-01 11:29:56 +0900

    Create 11399.py

---
+++ 백준/class3/11399.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+spending_time = sorted(map(int, sys.stdin.readline().split()))
+
+# 그리디 알고리즘 적용
+# 대기 시간의 총 누적 합을 최소로 하려면, 인출하는데 가장 짧은 시간이 걸리는 사람을 제일 앞에, 오랜 시간이 걸리는 사람을 제일 뒤에 두어야 한다.
+
+total_waiting_time = sum(time * (n - idx) for idx, time in enumerate(spending_time))
+
+print(total_waiting_time)
