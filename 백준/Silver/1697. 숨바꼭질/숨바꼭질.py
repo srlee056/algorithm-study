656980a44eb857bc83f329287f97345116704a95
이서림 <40015447+srlee056@users.noreply.github.com>
2024-01-18 02:51:29 +0000

    [Silver I] Title: 숨바꼭질, Time: 156 ms, Memory: 43624 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+
+n, k = map(int, sys.stdin.readline().split())
+
+visited = {n: True}
+position = deque([{n}])
+count = 0
+while position:
+    cur_set = position.popleft()
+
+    if k in cur_set:
+        print(count)
+        break
+    else:
+        next_set = set()
+        for cur in cur_set:
+            visited[cur] = True
+            if (cur - 1) not in visited and cur - 1 >= 0:
+                next_set.add(cur - 1)
+            if (cur + 1) not in visited and cur + 1 <= 100000:
+                next_set.add(cur + 1)
+            if (cur * 2) not in visited and cur * 2 <= 100000:
+                next_set.add(cur * 2)
+        position.append(next_set)
+        count += 1
