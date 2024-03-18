6e98c876985afc5351f4d7b56d2220e380a02307
Lee Sarah <imsolem1226@gmail.com>
2024-03-18 10:19:05 +0900

    -

--- 백준/class4/1167.py
+++ 백준/class4/1167.py
@@ -1,18 +1,16 @@
 import sys
-from collections import deque
 
 
 def dfs(node, tree, visited):
     max_node, max_cost = 0, 0
     for adj_node, cost in tree[node]:
-        # print(visited)
-        # print(adj_node, cost)
-        if not visited[adj_node] and cost != -1:
+        if not visited[adj_node]:
             visited[adj_node] = True
             d_node, d_cost = dfs(adj_node, tree, visited)
 
-            if d_cost != 0 and cost + d_cost > max_cost:
-                max_node, max_cost = d_node, cost + d_cost
+            if d_node and d_cost:  # 둘 다 0이 아님
+                if cost + d_cost > max_cost:
+                    max_node, max_cost = d_node, cost + d_cost
             elif cost > max_cost:
                 max_node, max_cost = adj_node, cost
     return (max_node, max_cost)
@@ -27,17 +25,16 @@ if __name__ == "__main__":
     for i in range(n):
         line = list(map(int, input().split()))
 
-        # print(f"v: {line[0]}")
         cur_node = line[0]
         for adj_node, cost in zip(line[1::2], line[2::2]):
             tree[cur_node].append((adj_node, cost))
-
+    # print(tree)
     # 1번 노드로부터 제일 먼 노드 찾기
     visited = [False for _ in range(n + 1)]
     visited[1] = True
-    # print(n, visited)
+
     far_node, c = dfs(1, tree, visited)
-    print(far_node, c)
+    # print(far_node, c)
     visited = [False for _ in range(n + 1)]
-    visited[n] = True
+    visited[far_node] = True
     print(dfs(far_node, tree, visited)[1])
