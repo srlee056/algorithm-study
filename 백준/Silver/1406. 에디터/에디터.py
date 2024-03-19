189cf9a0a2e40666857f22dbec612343773ab3e9
이서림 <40015447+srlee056@users.noreply.github.com>
2024-03-19 13:24:16 +0900

    [Silver II] Title: 에디터, Time: 288 ms, Memory: 38804 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+cursor_left = list(input().strip())
+cursor_right = deque([])
+
+# print(cursor_left)
+command_num = int(input())
+
+for _ in range(command_num):
+    command = input().split()
+    if command[0] == "L":
+        if cursor_left:
+            cursor_right.appendleft(cursor_left.pop())
+    elif command[0] == "D":
+        if cursor_right:
+            cursor_left.append(cursor_right.popleft())
+    elif command[0] == "B":
+        if cursor_left:
+            cursor_left.pop()
+    elif command[0] == "P":
+        cursor_left.append(command[1])
+
+print("".join(cursor_left) + "".join(cursor_right))
