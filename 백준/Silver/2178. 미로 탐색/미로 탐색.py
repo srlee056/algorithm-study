f166522c2a6d4b9968944ee1eebaa5233ac09a69
Lee Sarah <imsolem1226@gmail.com>
2024-03-19 12:24:52 +0900

    Update 미로 탐색.py

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,40 +1,37 @@
 import sys
 from collections import deque
 
-n, m = 0, 0
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 
+def bfs(start_node, maze_array):
+    n, m = len(maze_array), len(maze_array[0])
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    visited = [[False for _ in range(m)] for _ in range(n)]
+    visited[start_node[0]][start_node[1]] = True
+    nodes = deque([start_node])
 
-def bfs(nodes, maze_array, visited):
-    global n, m
-    dists = []
     while nodes:
         y, x, dist = nodes.popleft()
         if (y, x) == (n - 1, m - 1):
-            dists.append(dist)
-            continue
+            return dist
         for i in range(4):
             adj_y, adj_x = y + dy[i], x + dx[i]
 
-            if adj_y < 0 or adj_y >= n or adj_x < 0 or adj_x >= m:
-                continue
-            if visited[adj_y][adj_x]:
-                continue
-            if maze_array[adj_y][adj_x] == 0:
-                continue
-
-            nodes.append([adj_y, adj_x, dist + 1])
-            if (y, x) != (n - 1, m - 1):
+            if (
+                0 <= adj_y < n
+                and 0 <= adj_x < m
+                and not visited[adj_y][adj_x]
+                and maze_array[adj_y][adj_x] == 1
+            ):
+                # 첫번째로 도착한 거리가 최단거리이므로, 도착점을 여러번 방문하는 것에 대해 고려할 필요 없음.
                 visited[adj_y][adj_x] = True
-    # print(dists)
-    return min(dists)
+                nodes.append([adj_y, adj_x, dist + 1])
+
+    return -1
 
 
 def main():
-    global n, m
     input = sys.stdin.readline
-
     n, m = map(int, input().split())
 
     maze_array = []
@@ -42,12 +39,7 @@ def main():
         input_list = list(map(int, input().strip()))
         maze_array.append(input_list)
 
-    # print(maze_array)
-
-    visited = [[False for _ in range(m)] for _ in range(n)]
-    visited[0][0] = True
-
-    print(bfs(deque([[0, 0, 1]]), maze_array, visited))
+    print(bfs(deque([0, 0, 1]), maze_array))
 
 
 if __name__ == "__main__":
