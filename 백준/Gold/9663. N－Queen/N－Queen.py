0da7ccef8dcd96ef30f070ce3b4f8945650dd26a
이서림 <40015447+srlee056@users.noreply.github.com>
2024-03-21 12:08:37 +0900

    [Gold IV] Title: N-Queen, Time: 23588 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,43 @@
+import sys
+
+
+def backtrack(n, col, rows, diagonals):
+    if n == 1 and col == 0:
+        return 1
+    if col >= n:
+        return 1
+    result = 0
+    for row in range(n):
+
+        if rows[row] and diagonals[0][col + row] and diagonals[1][col - row + (n - 1)]:
+            rows[row] = False
+            diagonals[0][col + row] = False
+            diagonals[1][col - row + (n - 1)] = False
+        else:
+            continue
+
+        case = backtrack(n, col + 1, rows, diagonals)
+
+        rows[row] = True
+        diagonals[0][col + row] = True
+        diagonals[1][col - row + (n - 1)] = True
+        # print(case)
+        if case > 0:
+            result += case
+
+    return result
+
+
+def main():
+    input = sys.stdin.readline
+
+    n = int(input().strip())
+
+    rows = [True for _ in range(n)]
+    diagonals = [[True for _ in range(2 * n - 1)] for _ in range(2)]
+    sum_cases = backtrack(n, 0, rows, diagonals)
+    print(sum_cases)
+
+
+if __name__ == "__main__":
+    main()
