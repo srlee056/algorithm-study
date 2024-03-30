48f035b4d3b8e60de3b20ef1ca0fc5dd12796c79
이서림 <40015447+srlee056@users.noreply.github.com>
2024-03-30 19:37:29 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().strip())
+
+wh_list = []
+for i in range(n):
+    weight, height = map(int, input().split())
+    wh_list.append((weight, height, i))
+
+wh_sorted = sorted(wh_list, reverse=True)
+
+# print(wh_sorted)
+
+
+ranks = [1 for _ in range(n)]
+
+for i in range(n):
+    for j in range(i + 1, n):
+        w1, h1, i1 = wh_list[i]
+        w2, h2, i2 = wh_list[j]
+        if w1 < w2 and h1 < h2:
+            ranks[i1] += 1
+        elif w1 > w2 and h1 > h2:
+            ranks[i2] += 1
+# print(ranks)
+print(" ".join(map(str, ranks)))
