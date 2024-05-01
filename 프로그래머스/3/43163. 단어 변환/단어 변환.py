1806a56694e3f6cb6030fece9e97aa8412b402c4
이서림 <40015447+srlee056@users.noreply.github.com>
2024-05-01 11:23:48 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,52 +1,41 @@
-from collections import deque, defaultdict
+from collections import defaultdict, deque
+
 def solution(begin, target, words):
     
+    n = len(words)
     if target not in words:
         return 0
     
-    n = len(begin)
-    word_dict = defaultdict(set)
+    visited = {word: False for word in words}
     
-    for word in [begin] + words:
-        for word_ in words:
-            if word != word_ and can_transform(word, word_):
-                word_dict[word].add(word_)
-                word_dict[word_].add(word)
-    
-    #print(word_dict)
-    
-    visited = {word:False for word in words+[begin]}
-    
-    return dfs(begin, target, word_dict, visited)
-
-def can_transform(word_from, word_to):
-    n = len(word_from)
-    diff = 0
-    for i in range(n):
-        if word_from[i] != word_to[i]:
-            diff += 1
-        
-        if diff > 1:
-            return False
-    
-    return True
-    
-
-def dfs(word, target, word_dict, visited):
-    result = 100
-    if word == target:
-        return 0
-    if visited[word]:
-        return result
-    
-    #print(word)
-    trans_words = word_dict[word]
-    #print(trans_words)
-    visited[word] = True
-
-    for trans_word in trans_words:
-        if not visited[trans_word]:
-            #print(word, trans_word)
-            result = min(result, dfs(trans_word, target, word_dict, visited) + 1)
+    result = dfs(begin, target, words, visited, count=0)
     
     return result
+
+def dfs(begin, target, words, visited, count):
+    n = len(words)
+    
+    result = n+1
+    for i, word in enumerate(words):
+        if not visited[word] and can_transform(begin, word):
+            if word == target:
+                return count + 1
+            else :
+                visited[word] = True
+                result = min(result, dfs(word, target, words, visited, count+1))
+                visited[word] = False
+    
+    return result if result != n+1 else 0
+            
+def can_transform(word1, word2):
+    n = len(word1)
+        
+    count = 0
+    for i in range(n):
+        if word1[i] != word2[i]:
+            count +=1
+        if count > 2:
+            return False
+    
+    return True if count == 1 else False
+    
