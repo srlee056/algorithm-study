6dcbade76e6f9682a93ce156cc71eabeed82777a
이서림 <40015447+srlee056@users.noreply.github.com>
2024-05-09 13:23:56 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -3,26 +3,17 @@ from collections import defaultdict
 def solution(skill, skill_trees):
 
     count = 0
+    
     for skill_tree in skill_trees:
-        curr = 0
-        hasAppeared = {k : False for k in skill}
-        
-        for s in skill_tree:
-            if s in hasAppeared:
-                
-                hasAppeared[s] = True
-            if curr < len(skill) and s == skill[curr]:
-                curr += 1
-        
-        #print(hasAppeared)
-        #print(curr, [k for k, x in hasAppeared.items() if x])
-        if curr == len( [k for k, x in hasAppeared.items() if x]):
-            
+        cur_idx = 0
+        is_valid = True
+        for skill_ in skill_tree:
+            if skill_ in skill:
+                if skill_ != skill[cur_idx]:
+                    is_valid = False
+                    break
+                else:
+                    cur_idx += 1
+        if is_valid:
             count += 1
-        
-                
-            
-        
-        
-        
     return count
