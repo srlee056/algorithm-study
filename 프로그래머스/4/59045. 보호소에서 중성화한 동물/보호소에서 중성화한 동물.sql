e0f4e303d211e1185eeaf9f0c79417562d8cc119
이서림 <40015447+srlee056@users.noreply.github.com>
2024-05-11 09:33:53 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,5 @@
+select ai.ANIMAL_ID, ai.ANIMAL_TYPE, ai.NAME
+from ANIMAL_INS ai
+JOIN ANIMAL_OUTS ao
+ON ai.ANIMAL_ID = ao.ANIMAL_ID
+WHERE ai.SEX_UPON_INTAKE LIKE 'Intact%' and (ao.SEX_UPON_OUTCOME LIKE 'Spayed%' or ao.SEX_UPON_OUTCOME LIKE 'Neutered%')
